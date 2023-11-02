2079d4b949bbf66d182da4fce8cb943b7585f9e8
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-02 11:32:09 -0700

    add character age to form

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ window.onload = function () {
   let form = document.querySelector("form");
   form.onsubmit = function (event) {
 
-    const inputCharacter = document.getElementById("inputCharacter").value;
+    const inputCharacterName = document.getElementById("inputCharacterName").value;
+    const inputCharacterAge = document.getElementById("inputCharacterAge").value;
 
-    document.querySelector("span#userInput1").innerText = inputCharacter;
+    document.querySelector("span#userInput1").innerText = inputCharacterName;
+    document.querySelector("span#userInput2").innerText = inputCharacterAge.toString();
     document.querySelector("div#character").removeAttribute("class");
     event.preventDefault();
   };
